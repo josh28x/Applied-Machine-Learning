9efdf06c487e392eb21988ccc127653d1de99399
josh28x <joshua.programming.java@gmail.com>
2025-03-16 14:43:56 +1100

    added DataFrame

--- pandas_intro.py
+++ pandas_intro.py
@@ -2,8 +2,29 @@ import pandas as pd
 
 # initalise data of lists
 
-data = {'Name': ['Tom', 'nick', 'jack'],
+data = {'Name': ['Tom', 'nick','krish', 'jack'],
         'Age': [20, 21, 19, 18]}
 
 print(data)
 # output: {'Name': ['Tom', 'nick', 'jack'], 'Age': [20, 21, 19, 18]}
+
+# Create DataFrame
+
+df = pd.DataFrame(data)
+print(df)
+
+#experienced error because each row (in this case, Name and Age) in a DataFrame must have the same number of elements
+
+
+"""
+output of the DataFrame:
+
+    Name  Age
+0    Tom   20
+1   nick   21
+2  krish   19
+3   jack   18
+
+
+
+"""
