51ce0d150d1f08b9840c22e5079f781a0bc2587c
josh28x <joshua.programming.java@gmail.com>
2025-03-16 14:30:48 +1100

    added intro to pandas

---
+++ pandas_intro.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+# initalise data of lists
+
+data = {'Name': ['Tom', 'nick', 'jack'],
+        'Age': [20, 21, 19, 18]}
+
+print(data)
+# output: {'Name': ['Tom', 'nick', 'jack'], 'Age': [20, 21, 19, 18]}
